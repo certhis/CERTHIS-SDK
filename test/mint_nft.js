4da60c07b49d81118597a89dbcaddcd48cdb8c55
ruben-certhis <ruben@certhis.io>
2023-09-28 16:01:08 +0300

    fix queue

--- test/mint_nft.js
+++ test/mint_nft.js
@@ -14,29 +14,41 @@ async function initWalletConnection(rpc, wallet_pass) {
   });
 }
 
+
+async function mint(web3Inject,mint_wallet){
+
+  var nft_minted = await certhis.nft.mint({
+    web3: web3Inject,
+    to_wallet: mint_wallet,
+    collection_index: 1342,
+    nft: {
+      owner_address: mint_wallet,
+      token_uri:
+        "https://certhis.mypinata.cloud/ipfs/QmSRE5iGJCnNMti5t6RukoUxPccX3ouDRwKx7Sy5jupcNR/28.json",
+    },
+    price_value: 0,
+    mulParam: 2,
+  });
+
+  console.log("nft minted", nft_minted.nft_object.nft_id);
+}
+
 async function launch() {
-  let web3Inject = await initWalletConnection(
-    "https://polygon-mumbai.infura.io/v3/56e93ff3382e494782c4d6e2d8d1c902",
-    "0xc0afa1596a43442642db9be18378ce253d2d9539eb3474b87ff0d66bafb664a1"
-  );
-  var mint_wallet = "0x883f9048236a7Ab0DB1e352fe27760830BcC53B9";
+
+
+let web3Inject = await initWalletConnection(
+  "https://polygon-mumbai.infura.io/v3/56e93ff3382e494782c4d6e2d8d1c902",
+  "0xc0afa1596a43442642db9be18378ce253d2d9539eb3474b87ff0d66bafb664a1"
+);
+var mint_wallet = "0x883f9048236a7Ab0DB1e352fe27760830BcC53B9";
+
 
   try {
     //execute 10 times
     for (let i = 0; i < 50; i++) {
       try {
-        var nft_minted = certhis.nft.mint({
-          web3: web3Inject,
-          to_wallet: mint_wallet,
-          collection_index: 1342,
-          nft: {
-            owner_address: mint_wallet,
-            token_uri:
-              "https://certhis.mypinata.cloud/ipfs/QmSRE5iGJCnNMti5t6RukoUxPccX3ouDRwKx7Sy5jupcNR/28.json",
-          },
-          price_value: 0,
-          mulParam: 2,
-        });
+        console.log("mint in process for " + mint_wallet, i);
+        mint(web3Inject,mint_wallet);
       } catch (e) {
         console.log(e);
       }
@@ -45,7 +57,7 @@ async function launch() {
     console.log(e);
   }
 
-  return true;
+
 }
 
 launch();
